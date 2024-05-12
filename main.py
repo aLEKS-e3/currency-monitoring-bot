e9677ca2d4fe7acab24ca296890942e0e41480f6
aLEKS-e3 <chebukin404@gmail.com>
2024-05-12 13:30:01 +0300

    Add main.py with all necessary dependencies for launching the program

---
+++ main.py
@@ -0,0 +1,40 @@
+import asyncio
+import logging
+import time
+import schedule
+import sqlite3
+import sys
+from datetime import datetime
+
+from database import manage_db, get_xlsx_file
+from parse import parse_usd_to_uah_rate
+from telegram import main
+
+
+def gather_data() -> None:
+    date = datetime.now()
+    rate = parse_usd_to_uah_rate()
+
+    with sqlite3.connect("usd-to-uah-rates.db") as conn:
+        cur = conn.cursor()
+
+        manage_db(conn, cur, date, rate)
+        get_xlsx_file(conn, date.date())
+
+        cur.close()
+
+
+def launch() -> None:
+    schedule.every().hour.do(gather_data)
+
+    logging.basicConfig(level=logging.DEBUG, stream=sys.stdout)
+    gather_data()
+    asyncio.run(main())
+
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+
+if __name__ == "__main__":
+    launch()
