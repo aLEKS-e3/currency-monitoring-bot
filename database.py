f94a287e5cbb0cac9f4073f0ff3938af7080a5ec
aLEKS-e3 <chebukin404@gmail.com>
2024-05-12 13:28:49 +0300

    Implement logic for inserting data into db + converting it into xlsx file

---
+++ database.py
@@ -0,0 +1,20 @@
+from datetime import date
+from sqlite3 import Connection
+
+import pandas as pd
+
+
+def manage_db(conn, cur, date, rate) -> None:
+    # cur.execute("CREATE TABLE rates(datetime, exchange_rate)")
+    cur.execute("INSERT INTO rates VALUES(?, ?);", (date, rate))
+
+    conn.commit()
+
+
+def get_xlsx_file(conn: Connection, today: date) -> None:
+    today_data = pd.read_sql_query(
+        f"SELECT * FROM rates WHERE DATE(datetime) = '{today}'",
+        conn
+    )
+    filename = f"{today}-usd-uah-rate.xlsx"
+    today_data.to_excel(filename, index=False)
