cc8dc0935be172dbf2520f5ac9c7e093d24903f3
aLEKS-e3 <chebukin404@gmail.com>
2024-05-12 13:34:00 +0300

    Move url variable to main.py

--- parse.py
+++ parse.py
@@ -1,16 +1,9 @@
-import os
-
 import requests
 from bs4 import BeautifulSoup
-from dotenv import load_dotenv
-
-load_dotenv()
-
-URL = os.getenv("URL")
 
 
-def parse_usd_to_uah_rate() -> str:
-    page = requests.get(URL)
+def parse_usd_to_uah_rate(url: str) -> str:
+    page = requests.get(url)
     soup = BeautifulSoup(page.content, "html.parser")
     rate = soup.select_one(
         "div > div > div > div:nth-child(1) > div > span > div > div"
