9efbcd8c4382e08ef5503102a6f88d6b68631ab2
aLEKS-e3 <chebukin404@gmail.com>
2024-05-13 14:22:21 +0300

    Move file name to a constant

--- database.py
+++ database.py
@@ -4,6 +4,9 @@ from sqlite3 import Connection
 import pandas as pd
 
 
+FILE = "{date}-usd-uah-rate.xlsx"
+
+
 def manage_db(conn, cur, date, rate) -> None:
     cur.execute("CREATE TABLE IF NOT EXISTS rates(datetime, exchange_rate)")
     cur.execute("INSERT INTO rates VALUES(?, ?);", (date, rate))
@@ -16,5 +19,5 @@ def get_xlsx_file(conn: Connection, today: date) -> None:
         f"SELECT * FROM rates WHERE DATE(datetime) = '{today}'",
         conn
     )
-    filename = f"{today}-usd-uah-rate.xlsx"
+    filename = FILE.format(date = today)
     today_data.to_excel(filename, index=False)
